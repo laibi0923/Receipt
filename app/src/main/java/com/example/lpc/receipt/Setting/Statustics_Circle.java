ecfb7d05f0cb4834e5bec7d9d2df664f3ffc7259
Laibi0923 <laibi0923@gmail.com>
2019-02-03 23:00:22 +0800

    djdhdhd

---
+++ app/src/main/java/com/example/lpc/receipt/Setting/Statustics_Circle.java
@@ -0,0 +1,14 @@
+package com.example.lpc.receipt.Setting;
+import android.util.*;
+
+public class Statustics_Circle
+{
+
+	public Statustics_Circle(){
+		
+		
+		
+	}
+
+	
+}
--- app/src/main/java/com/example/lpc/receipt/Setting/Statustics_Main.java
+++ app/src/main/java/com/example/lpc/receipt/Setting/Statustics_Main.java
@@ -62,6 +62,7 @@ public class Statustics_Main extends AppCompatActivity {
 
 
 	private void Circle_Statustics(){
+		
 		// 螢幕長度
 		int phone_width = Get_Window_Width();
 		// 螢幕長度 - 80dp (marginLeft, marginRight)
@@ -81,103 +82,80 @@ public class Statustics_Main extends AppCompatActivity {
 		// 統計條寬 (綠)
 		int statustics_bar2_width = 30;
 
-
-
 		statustics_view.getLayoutParams().height = setup_height_width;
 		statustics_view.getLayoutParams().width = setup_height_width;
 
         Log.e("Phone_Width", phone_width + "px, " + PX_convert_DP(phone_width) + "dp");
         Log.e("setup_height_width", setup_height_width + "px, " + PX_convert_DP(setup_height_width) + "dp");
 
-
+		
+		// LinearyLayout
+		// 	- TextView
+		// 	- RelativeLayout
+		// 		- View (baseline)
+		// 		- View (Bar 1)
+		//		- View (Bar 2)
 
 
 		for (int i = 0; i < line_count; i++){
-
-			// 主要容器
-			RelativeLayout mRelativeLayout = new RelativeLayout(this);
-			// 設定長度 寬度
-			RelativeLayout.LayoutParams mRelativeLayout_LayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, statustics_bar_maxheight);
-			// 置中
-			mRelativeLayout_LayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-
-			mRelativeLayout.setLayoutParams(mRelativeLayout_LayoutParams);
-
-
+			
+			LinearLayout mLinearLayout = new LinearLayout(this);
+			mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+			RelativeLayout.LayoutParams pp1 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, statustics_bar_maxheight);
+			pp1.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+			mLinearLayout.setLayoutParams(pp1);
+			statustics_view.addView(mLinearLayout);
+			
 			// 標題
 			TextView mTextView = new TextView(this);
-
 			mTextView.setText(mths_text_cht[i]);
-
 			mTextView.setTextColor(getResources().getColor(R.color.text_color_2));
-
 			RelativeLayout.LayoutParams Textview_LayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-
 			Textview_LayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-
 			Textview_LayoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
-
 			Textview_LayoutParams.setMargins(0, 20, 0, 0);
-
 			mTextView.setLayoutParams(Textview_LayoutParams);
+			mLinearLayout.addView(mTextView);
 
-			mRelativeLayout.addView(mTextView);
-
-
+			
+			RelativeLayout mRelativeLayout = new RelativeLayout(this);
+			RelativeLayout.LayoutParams mRelativeLayout_LayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+			mRelativeLayout_LayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+			mRelativeLayout.setLayoutParams(mRelativeLayout_LayoutParams);
+			mLinearLayout.addView(mRelativeLayout);
+			
 
 			// Statustics Line
 			View statustics_line = new View(this);
-
 			statustics_line.setBackgroundColor(getResources().getColor(R.color.text_color_2));
-
 			RelativeLayout.LayoutParams line_LayoutParams = new RelativeLayout.LayoutParams(base_line_width, base_line_height);
-
 			line_LayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-
 			line_LayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-
 			statustics_line.setLayoutParams(line_LayoutParams);
-
 			mRelativeLayout.addView(statustics_line);
 
 
 			// 統計條 (白)
 			View statustics_bar1 = new View(this);
-
 			statustics_bar1.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-
 			int Random_Num1 = (int) (Math.random() * 100 +100);
-
 //			Log.e("RandomNum", Random_Num1 + "");
-
 			RelativeLayout.LayoutParams statustics_bar1_LayoutPams = new RelativeLayout.LayoutParams(statustics_bar1_width, Random_Num1);
-
 			statustics_bar1_LayoutPams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-
 			statustics_bar1_LayoutPams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-
 			statustics_bar1.setLayoutParams(statustics_bar1_LayoutPams);
-
 			mRelativeLayout.addView(statustics_bar1);
 
 
 			// 統計條 (綠)
 			View statustics_bar2 = new View(this);
-
 			statustics_bar2.setBackgroundColor(getResources().getColor(R.color.text_color_1));
-
 			int Random_Num2 = (int) (Math.random() * 100  );
-
 //			Log.e("RandomNum", Random_Num2 + "");
-
 			RelativeLayout.LayoutParams statustics_bar2_LayoutPams = new RelativeLayout.LayoutParams(statustics_bar2_width, Random_Num2);
-
 			statustics_bar2_LayoutPams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-
 			statustics_bar2_LayoutPams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-
 			statustics_bar2.setLayoutParams(statustics_bar2_LayoutPams);
-
 			mRelativeLayout.addView(statustics_bar2);
 
 
@@ -187,54 +165,28 @@ public class Statustics_Main extends AppCompatActivity {
 			// Calculate the angle of the current view. Adjust by 90 degrees to
 			// get View 0 at the top. We need the angle in degrees and radians.
 			float angleDeg = i * 250.0f / line_count - 90.0f;
-
 			float angleRad = (float)(angleDeg * Math.PI / 180.0f);
 
 			// Calculate the position of the view, offset from center (300 px from
 			// center). Again, this should be done in a display size independent way.
-			mRelativeLayout.setTranslationX(center_circle_size * (float)Math.cos(angleRad));
+			mLinearLayout.setTranslationX(center_circle_size * (float)Math.cos(angleRad));
 
-			mRelativeLayout.setTranslationY(center_circle_size * (float)Math.sin(angleRad));
+			mLinearLayout.setTranslationY(center_circle_size * (float)Math.sin(angleRad));
 
 			// Set the rotation of the view.
-			mRelativeLayout.setRotation(angleDeg + 90.0f);
+			mLinearLayout.setRotation(angleDeg + 90.0f);
 
 			// 將文字旋轉到岩位
 			mTextView.setRotation(-angleDeg + 270.0f);
 
-            statustics_view.addView(mRelativeLayout);
+            //statustics_view.addView(mLinearLayout);
 
 		}
 
 	}
-
-//            // Crea>te some quick TextViews that can be placed.
-//            TextView v = new TextView(this);
-//            // Set a text and center it in each view.
-//            v.setText("View " + i);
-//            v.setGravity(Gravity.CENTER);
-//            v.setBackgroundColor(0xffff0000);
-//            // Force the views to a nice size (150x100 px) that fits my display.
-//            // This should of course be done in a display size independent way.
-//            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(150, 100);
-//            // Place all views in the center of the layout. We'll transform them
-//            // away from there in the code below.
-//            lp.gravity = Gravity.CENTER;
-//            // Set layout params on view.
-//            v.setLayoutParams(lp);
-//
-//            // Calculate the angle of the current view. Adjust by 90 degrees to
-//            // get View 0 at the top. We need the angle in degrees and radians.
-//            float angleDeg = i * 360.0f / numViews - 90.0f;
-//            float angleRad = (float)(angleDeg * Math.PI / 180.0f);
-//            // Calculate the position of the view, offset from center (300 px from
-//            // center). Again, this should be done in a display size independent way.
-//            v.setTranslationX(300 * (float)Math.cos(angleRad));
-//            v.setTranslationY(300 * (float)Math.sin(angleRad));
-//            // Set the rotation of the view.
-//            v.setRotation(angleDeg + 90.0f);
-//            main.addView(v);
-
+	
+	
+	
 
 }
 
