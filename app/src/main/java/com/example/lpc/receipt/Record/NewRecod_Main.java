fe703b078d3ecf0b0df6341ee39d9d30a8f7620b
laibi0923 <laibi0923@gmail.com>
2019-03-26 00:11:14 +0800

    Update on 26 Mar 2019

--- app/src/main/java/com/example/lpc/receipt/Record/NewRecod_Main.java
+++ app/src/main/java/com/example/lpc/receipt/Record/NewRecod_Main.java
@@ -1,23 +1,28 @@
 package com.example.lpc.receipt.Record;
+import android.graphics.Color;
 import android.os.*;
 import android.support.v4.app.*;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.*;
 import android.support.v7.app.*;
 import android.view.*;
 import android.widget.*;
 import com.example.lpc.receipt.*;
 import android.view.View.*;
-public class NewRecod_Main extends AppCompatActivity
+public class NewRecod_Main extends AppCompatActivity implements NewRecord_Keybord.Keyboard_Listener
 {
-	private LinearLayout newrecord_close_btn;
+	private RelativeLayout newrecord_close_btn;
+
+	private EditText newrecord_amount_ed;
 	
 	private ViewPager mViewPager;
-	
-	//private TabLayout mTabLayout;
+
+	private RelativeLayout newrecord_keyboard_btn, newrecord_option1_btn, newrecord_option2_btn, newrecord_option3_btn, newrecord_option4_btn,newrecord_save_btn;
+
+	private ImageView newrecord_keyboard_img, newrecord_option1_img, newrecord_option2_img, newrecord_option3_img, newrecord_option4_img,newrecord_save_img;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.c001_new_record);
@@ -27,15 +32,95 @@ public class NewRecod_Main extends AppCompatActivity
 	
 	private void Find_View(){
 		
-		newrecord_close_btn = (LinearLayout) findViewById(R.id.newrecord_close_btn);
+		newrecord_close_btn = (RelativeLayout) findViewById(R.id.newrecord_close_btn);
 		newrecord_close_btn.setOnClickListener(Item_OnclickListener);
-		
+
+		newrecord_amount_ed = findViewById(R.id.newrecord_amount_ed);
+
 		mViewPager = (ViewPager) findViewById(R.id.newrecord_viewpage);
 		
 		Viewpager_Adapter mViewpager_Adapter = new Viewpager_Adapter(getSupportFragmentManager());
 		
 		mViewPager.setAdapter(mViewpager_Adapter);
+
+		mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override
+			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+			}
+
+			@Override
+			public void onPageSelected(int position) {
+
+				switch (position){
+
+					case 0:
+						Clear_Icon_Tint();
+						newrecord_keyboard_img.setColorFilter(Color.parseColor("#FFFFFF"));
+						break;
+
+					case 1:
+						Clear_Icon_Tint();
+						newrecord_option1_img.setColorFilter(Color.parseColor("#FFFFFF"));
+						break;
+
+					case 2:
+						Clear_Icon_Tint();
+						newrecord_option2_img.setColorFilter(Color.parseColor("#FFFFFF"));
+						break;
+
+					case 3:
+						Clear_Icon_Tint();
+						newrecord_option3_img.setColorFilter(Color.parseColor("#FFFFFF"));
+						break;
+
+					case 4:
+						Clear_Icon_Tint();
+						newrecord_option4_img.setColorFilter(Color.parseColor("#FFFFFF"));
+						break;
+
+				}
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int state) {
+
+			}
+		});
+
+		newrecord_keyboard_btn = findViewById(R.id.newrecord_keyboard_btn);
+		newrecord_keyboard_btn.setOnClickListener(Item_OnclickListener);
+		newrecord_keyboard_img = findViewById(R.id.newrecord_keyboard_img);
+
+		newrecord_option1_btn = findViewById(R.id.newrecord_option1_btn);
+		newrecord_option1_btn.setOnClickListener(Item_OnclickListener);
+		newrecord_option1_img = findViewById(R.id.newrecord_option1_img);
+
+		newrecord_option2_btn = findViewById(R.id.newrecord_option2_btn);
+		newrecord_option2_btn.setOnClickListener(Item_OnclickListener);
+		newrecord_option2_img = findViewById(R.id.newrecord_option2_img);
+
+		newrecord_option3_btn = findViewById(R.id.newrecord_option3_btn);
+		newrecord_option3_btn.setOnClickListener(Item_OnclickListener);
+		newrecord_option3_img = findViewById(R.id.newrecord_option3_img);
+
+		newrecord_option4_btn = findViewById(R.id.newrecord_option4_btn);
+		newrecord_option4_btn.setOnClickListener(Item_OnclickListener);
+		newrecord_option4_img = findViewById(R.id.newrecord_option4_img);
+
+		newrecord_save_btn = findViewById(R.id.newrecord_save_btn);
+		newrecord_save_btn.setOnClickListener(Item_OnclickListener);
+		newrecord_save_img = findViewById(R.id.newrecord_save_img);
 		
+	}
+
+	private void Clear_Icon_Tint(){
+		newrecord_keyboard_img.setColorFilter(Color.parseColor("#2E2E2E"));
+		newrecord_option1_img.setColorFilter(Color.parseColor("#2E2E2E"));
+		newrecord_option2_img.setColorFilter(Color.parseColor("#2E2E2E"));
+		newrecord_option3_img.setColorFilter(Color.parseColor("#2E2E2E"));
+		newrecord_option4_img.setColorFilter(Color.parseColor("#2E2E2E"));
+		newrecord_save_img.setColorFilter(Color.parseColor("#2E2E2E"));
 	}
 	
 	private View.OnClickListener Item_OnclickListener = new View.OnClickListener(){
@@ -50,14 +135,62 @@ public class NewRecod_Main extends AppCompatActivity
 				case R.id.newrecord_close_btn:
 					finish();
 					break;
+
+				case R.id.newrecord_keyboard_btn:
+
+					mViewPager.setCurrentItem(0);
+					Clear_Icon_Tint();
+					newrecord_keyboard_img.setColorFilter(Color.parseColor("#FFFFFF"));
+					break;
+
+				case R.id.newrecord_option1_btn:
+
+					mViewPager.setCurrentItem(1);
+					Clear_Icon_Tint();
+					newrecord_option1_img.setColorFilter(Color.parseColor("#FFFFFF"));
+					break;
+
+				case R.id.newrecord_option2_btn:
+
+					mViewPager.setCurrentItem(2);
+					Clear_Icon_Tint();
+					newrecord_option2_img.setColorFilter(Color.parseColor("#FFFFFF"));
+					break;
+
+				case R.id.newrecord_option3_btn:
+
+					mViewPager.setCurrentItem(3);
+					Clear_Icon_Tint();
+					newrecord_option3_img.setColorFilter(Color.parseColor("#FFFFFF"));
+					break;
+
+				case R.id.newrecord_option4_btn:
+
+					mViewPager.setCurrentItem(4);
+					Clear_Icon_Tint();
+					newrecord_option4_img.setColorFilter(Color.parseColor("#FFFFFF"));
+					break;
+
+				case R.id.newrecord_save_btn:
+					break;
 			}
-			
+
 		}
 		
 	};
-	
-	public class Viewpager_Adapter extends FragmentPagerAdapter
-	{
+
+	@Override
+	public void SendContent(String Info) {
+		if (Info.equals("del")){
+			Toast.makeText(this, "del", Toast.LENGTH_SHORT).show();
+		}else {
+			newrecord_amount_ed.append(Info);
+		}
+	}
+
+	// ViewPager Adapter
+	public class Viewpager_Adapter extends FragmentPagerAdapter {
+
 		public Viewpager_Adapter(FragmentManager mFragmentManager){
 			super(mFragmentManager);
 		}
--- app/src/main/java/com/example/lpc/receipt/Record/NewRecord_Keybord.java
+++ app/src/main/java/com/example/lpc/receipt/Record/NewRecord_Keybord.java
@@ -1,19 +1,143 @@
 package com.example.lpc.receipt.Record;
+import android.content.Context;
 import android.support.v4.app.*;
 import android.view.*;
 import android.os.*;
+import android.widget.RelativeLayout;
 
 import com.example.lpc.receipt.R;
-public class NewRecord_Keybord extends Fragment
-{
+public class NewRecord_Keybord extends Fragment {
+
+	public Keyboard_Listener mKeyboard_Listener;
+
+	private RelativeLayout numberpad_del, numberpad_1, numberpad_2, numberpad_3, numberpad_4, numberpad_5, numberpad_6, numberpad_7,
+							numberpad_8, numberpad_9, numberpad_0, numberpad_00, numberpad_dec;
+
+	public interface Keyboard_Listener{
+		public void SendContent(String Info);
+	}
+
+	@Override
+	public void onAttach(Context context) {
+		super.onAttach(context);
+		mKeyboard_Listener = (Keyboard_Listener) getActivity();
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		// TODO: Implement this method
 		View v = inflater.inflate(R.layout.c001_newrecord_keybord, container, false);
+		Find_View(v);
 		return v;
 	}
+
+	private void Find_View(View v){
+
+		numberpad_del = v.findViewById(R.id.numberpad_del);
+		numberpad_del.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_1 = v.findViewById(R.id.numberpad_1);
+		numberpad_1.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_2 = v.findViewById(R.id.numberpad_2);
+		numberpad_2.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_3 = v.findViewById(R.id.numberpad_3);
+		numberpad_3.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_4 = v.findViewById(R.id.numberpad_4);
+		numberpad_4.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_5 = v.findViewById(R.id.numberpad_5);
+		numberpad_5.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_6 = v.findViewById(R.id.numberpad_6);
+		numberpad_6.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_7 = v.findViewById(R.id.numberpad_7);
+		numberpad_7.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_8 = v.findViewById(R.id.numberpad_8);
+		numberpad_8.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_9 = v.findViewById(R.id.numberpad_9);
+		numberpad_9.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_0 = v.findViewById(R.id.numberpad_0);
+		numberpad_0.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_00 = v.findViewById(R.id.numberpad_00);
+		numberpad_00.setOnClickListener(NumberPad_OnclickListener);
+
+		numberpad_dec = v.findViewById(R.id.numberpad_dec);
+		numberpad_dec.setOnClickListener(NumberPad_OnclickListener);
+
+	}
+
+	private View.OnClickListener NumberPad_OnclickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+
+			switch (v.getId()){
+
+				case R.id.numberpad_del:
+					mKeyboard_Listener.SendContent("del");
+					break;
+
+				case R.id.numberpad_1:
+					mKeyboard_Listener.SendContent("1");
+					break;
+
+				case R.id.numberpad_2:
+					mKeyboard_Listener.SendContent("2");
+					break;
+
+				case R.id.numberpad_3:
+					mKeyboard_Listener.SendContent("3");
+					break;
+
+				case R.id.numberpad_4:
+					mKeyboard_Listener.SendContent("4");
+					break;
+
+				case R.id.numberpad_5:
+					mKeyboard_Listener.SendContent("5");
+					break;
+
+				case R.id.numberpad_6:
+					mKeyboard_Listener.SendContent("6");
+					break;
+
+				case R.id.numberpad_7:
+					mKeyboard_Listener.SendContent("7");
+					break;
+
+				case R.id.numberpad_8:
+					mKeyboard_Listener.SendContent("8");
+					break;
+
+				case R.id.numberpad_9:
+					mKeyboard_Listener.SendContent("9");
+					break;
+
+				case R.id.numberpad_0:
+					mKeyboard_Listener.SendContent("0");
+					break;
+
+				case R.id.numberpad_00:
+					mKeyboard_Listener.SendContent("00");
+					break;
+
+				case R.id.numberpad_dec:
+					mKeyboard_Listener.SendContent(".");
+					break;
+
+
+			}
+
+		}
+	};
 	
 	
 }
